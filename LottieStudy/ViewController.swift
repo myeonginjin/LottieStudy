c0ed08c806256e6ee0a2b661a54fe62d53a1e82c
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2024-04-08 00:22:52 +0900

    Initial Commit

---
+++ LottieStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottieStudy
+//
+//  Created by 진명인 on 4/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
